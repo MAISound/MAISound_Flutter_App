c1415720f4ccb5e0a90b83d4d7ab46763a1fca33
Gabriel Mantovani <141573978+GabrielVY@users.noreply.github.com>
2024-11-24 23:38:07 -0300

    Arrumando conexao com API

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -3,19 +3,20 @@ import 'package:http/http.dart' as http;
 import 'package:maisound/classes/globals.dart';
 import 'package:maisound/classes/instrument.dart';
 import 'package:maisound/classes/track.dart';
+import 'package:maisound/services/user_service.dart';
 
 class ChatService {
   // URL base da API
   final String baseUrl = "http://localhost:5000";
+
+  UserService _userService = UserService();
 
   // Envia a mensagem para a API
   Future<Map<String, dynamic>> send(String prompt) async {
     final response = await http.post(
       Uri.parse('$baseUrl/api/auth/chat'),
 
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
+      headers: await _userService.getAuthHeader(),
       body: jsonEncode(<String, String>{
         "prompt": prompt,
       }),
